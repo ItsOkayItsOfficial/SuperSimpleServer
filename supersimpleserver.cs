0219859982dc39ae5829ec7085b221a966eaa440
Alex P <alexp@itsokayitsofficial.io>
2019-08-07 22:08:11 -0700

    Initialized

---
+++ supersimpleserver.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Net;
+
+namespace SuperSimpleServer
+{
+    public class Program {
+        // Entry - NOTE: start-up parameters are provided for, but won't be analyzed
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
